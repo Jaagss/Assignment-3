fbaca9f281300ea18cc6058a0aef5c629223f443
Jaagss <jagadaleaditya057@gmail.com>
2023-02-08 17:18:52 +0530

    Done

---
+++ Question_4.py
@@ -0,0 +1,152 @@
+with open("marks.txt",'r') as f:
+    a = f.read().splitlines()
+
+main_list=[]
+for i in a:
+    i = i.split(', ')
+    main_list.append(i)
+
+total_mark = [30,15,30,25]
+weightage = [('labs',30),('midsem',15),('assignments',30),('endsem',25)]
+policy = [80,65,50,40]
+total_student_marks = []
+
+for i in main_list:
+    sum_ = 0
+    for j in range(1,len(i)):
+        sum_ += int(i[j])
+    total_student_marks.append(sum_)
+
+
+class course:
+    def __init__(self,policy):
+        self.policy = policy
+        self.grade = ['A','B','C','D','F']
+    
+    def percentile(self):
+        percentile_list = []
+        for i in range(len(main_list)):
+            temp_list = 0
+            for j in range(1,len(main_list[i])):
+                temp_list += round((int(main_list[i][j]) / total_mark[j-1] * weightage[j-1][1]),2)
+            percentile_list.append(temp_list)
+        return percentile_list
+    
+    def cutoff_final(self):
+        final_percentile_list = []
+        percentile_list = course(self.policy).percentile()
+        for i in range(len(self.policy)):
+            temp_list = []
+            for j in percentile_list:
+                if j <= self.policy[i]+2 and j >= self.policy[i]-2:
+                    temp_list.append(j)
+            
+            temp_list.sort(reverse = True)
+            diff = 0
+            if temp_list == []:
+                pass
+            else:
+                for k in range(1,len(temp_list)):
+                    if diff < (temp_list[k-1] - temp_list[k]):
+                        diff = temp_list[k-1] - temp_list[k]
+                        v1 = temp_list[k-1]
+                        v2 = temp_list[k]
+                        final_percentile_list.append((v1+v2)/2)
+                if final_percentile_list == []:
+                    pass
+                else:
+                    
+                    self.policy[i] = final_percentile_list[i]
+
+    def grading(self):
+        percentile_list = course(self.policy).percentile()
+        self.final_grade = course(self.policy).percentile().copy()
+        for i in range(len(percentile_list)):
+            if percentile_list[i] > self.policy[0]:
+                self.final_grade[i] = self.grade[0]
+            
+            elif percentile_list[i] > self.policy[1]:
+                self.final_grade[i] = self.grade[1]
+
+            elif percentile_list[i] > self.policy[2]:
+                self.final_grade[i] = self.grade[2]
+            
+            elif percentile_list[i] > self.policy[3]:
+                self.final_grade[i] = self.grade[3]
+            
+            elif percentile_list[i] <= self.policy[3]:
+                self.final_grade[i] = self.grade[4]
+        
+        return self.final_grade
+        
+    
+    def counter(self):
+        A, B, C, D, F = 0, 0, 0, 0, 0
+
+        for i in self.final_grade:
+            if i == 'A':
+                A += 1
+            elif i == "B":
+                B += 1
+            elif i == 'C':
+                C += 1
+            elif i =='D':
+                D += 1
+            elif i == 'F':
+                F += 1
+        counter_list = []
+        counter_list.append(A)
+        counter_list.append(B)
+        counter_list.append(C)
+        counter_list.append(D)
+        counter_list.append(F)
+        return counter_list
+
+
+class Student:
+    def __init__(self,course_name,credit,weightage,policy,grade,counter):
+        self.course_name = course_name
+        self.credit = credit
+        self.weightage = weightage
+        self.policy = policy
+        self.grade = grade
+        self.counter = counter
+
+    def get_summary(self):
+        print("----------------------------------------")
+        print("                 SUMMARY                ")
+        print("                  ",self.course_name,self.credit)
+        print("Assessments -",self.weightage)
+        print("          Policy -",self.policy)
+        print("     A -",self.counter[0],"B -",self.counter[1],"C -",self.counter[2],"D -",self.counter[3],"F -",self.counter[4])
+
+    def grade_in_file(self):
+        with open('grade.txt','w') as f:
+            for i in range(len(main_list)):
+                f.write(main_list[i][0] + ', ' + str(total_student_marks[i]) + ', ' + str(self.grade[i]) + '\n')
+
+
+    def student_record(self,main_list):
+        rollno = str(input("Enter roll no: "))
+        for i in range(len(main_list)):
+            if str(main_list[i][0]) == rollno:
+                print(main_list[i][1::],end=", ")
+                print(total_student_marks[i],end=", ")
+                print(self.grade[i])
+
+
+course_class = course(policy)
+course_class.cutoff_final()
+course_class.grading()
+
+stu = Student('IP',"4",weightage,policy,course_class.grading(),course_class.counter())
+while True:
+    print("1. Generate grading summary\n2. Print grade of all students in file\n3. Search for a students record\n")
+    
+    choice = int(input("Enter choice: "))
+    if choice == 1:
+        stu.get_summary()
+    elif choice == 2:
+        stu.grade_in_file()
+    elif choice == 3:
+        stu.student_record(main_list)
---
+++ Question_5.py
@@ -0,0 +1,139 @@
+with open("marks.txt",'r') as f:
+    a = f.read().splitlines()
+
+main_list=[]
+for i in a:
+    i = i.split(', ')
+    main_list.append(i)
+
+total_mark = [30,15,30,25]
+weightage = [('labs',30),('midsem',15),('assignments',30),('endsem',25)]
+policy = [80,65,50,40]
+grade = ['A','B','C','D','F']
+
+total_student_marks = []
+for i in main_list:
+    sum_ = 0
+    for j in range(1,len(i)):
+        sum_ += int(i[j])
+    total_student_marks.append(sum_)
+
+
+def percentage(main,total_mark,weights):
+    percentile_list = []
+    for i in range(len(main)):
+        temp = 0
+        for j in range(1,len(main[i])):
+            temp += round((float(main[i][j])) / total_mark[j-1] * weights[j-1][1],2)
+        percentile_list.append(temp)
+    return percentile_list
+
+
+def final_cutoff(policy):
+    final_percentile_list = []
+    percentile_list = percentage(main_list,total_mark,weightage)
+    pol = policy.copy()
+    for i in range(len(policy)):
+        temp = []
+        for j in percentile_list:
+                if j <= pol[i]+2 and j >= pol[i]-2:
+                    temp.append(j)
+        temp.sort(reverse = True)
+        diff = 0
+        if temp == []:
+            pass
+        else:
+            for k in range(1,len(temp)):
+                if diff < (temp[k-1] - temp[k]):
+                    diff = temp[k-1] - temp[k]
+                    v1 = temp[k-1]
+                    v2 = temp[k]
+                    final_percentile_list.append((v1+v2)/2)
+            if final_percentile_list == []:
+                pass
+            else:
+                pol[i] = final_percentile_list[i]
+    return pol
+
+
+def grading(pol):
+    policy = final_cutoff(pol)
+    percentile_list = percentage(main_list,total_mark,weightage)
+    final_grade = percentage(main_list,total_mark,weightage).copy()
+    for i in range(len(percentile_list)):
+        if percentile_list[i] > policy[0]:
+            final_grade[i] = grade[0]
+        
+        elif percentile_list[i] > policy[1]:
+            final_grade[i] = grade[1]
+
+        elif percentile_list[i] > policy[2]:
+            final_grade[i] = grade[2]
+        
+        elif percentile_list[i] > policy[3]:
+            final_grade[i] = grade[3]
+        
+        elif percentile_list[i] <= policy[3]:
+            final_grade[i] = grade[4]
+    
+    return final_grade
+
+def counter_1(pol):
+    A, B, C, D, F = 0, 0, 0, 0, 0
+    final_grade = grading(pol)
+    for i in final_grade:
+        if i == 'A':
+            A += 1
+        elif i == "B":
+            B += 1
+        elif i == 'C':
+            C += 1
+        elif i =='D':
+            D += 1
+        elif i == 'F':
+            F += 1
+    counter_list = []
+    counter_list.append(A)
+    counter_list.append(B)
+    counter_list.append(C)
+    counter_list.append(D)
+    counter_list.append(F)
+    return counter_list
+
+counter = counter_1(policy)
+policy = final_cutoff(policy)
+def get_summary(cname, credit,policy):
+    print("----------------------------------------")
+    print("                 SUMMARY                ")
+    print("                  ",cname,credit)
+    print("Assessments -",weightage)
+    print("          Policy -",policy)
+    print("     A -",counter[0],"B -",counter[1],"C -",counter[2],"D -",counter[3],"F -",counter[4])
+
+
+def grade_in_file(main):
+    with open('grade.txt','w') as f:
+        for i in range(len(main)):
+            f.write(main[i][0] + ', ' + str(total_student_marks[i]) + ', ' + str(grade[i]) + '\n')
+
+
+def student_record(main_list):
+    rollno = str(input("Enter roll no: "))
+    for i in range(len(main_list)):
+        if str(main_list[i][0]) == rollno:
+            print(main_list[i][1::],end=", ")
+            print(total_student_marks[i],end=", ")
+            print(grade[i])
+
+while True:
+    print("1. Generate grading summary\n2. Print grade of all students in file\n3. Search for a students record\n")
+    
+    choice = int(input("Enter choice: "))
+    if choice == 1:
+        cname = input("Enter course name: ")
+        credit = input("Enter credits: ")
+        get_summary(cname,credit,policy)
+    elif choice == 2:
+        grade_in_file()
+    elif choice == 3:
+        student_record(main_list)
